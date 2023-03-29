689f06db58d7fc54c55b041e1f5c8248dfec0d6f
reaganmunene <reagannmunene@gmail.com>
2023-03-29 21:27:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let charge
+  if (distance <= '400'){
+    charge = 'This one is on me!'
+    return charge 
+  } else if (distance > '2000'){
+    charge = 'I will gladly take your thirty bucks.'
+    return charge
+  } else if (distance > '2500'){
+    charge = 'No can do'
+    return charge
+  }
+    
+  }
+
+  /*function scuberGreetingForFeet(distance){
+    let charge
+    if (distance >= '2000'){
+      charge = 'I will gladly take your thirty bucks.'
+      return charge 
+    }
+      
+    }*/
+
+
+
+function ternaryCheckCity(city){
+  
+  city = NYC? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  if(tip==='generous'){
+    response='Thank you so much.'
+  } else if(tip!=='generous'){
+    response='Thank you.'
+  } else response='Bye'
 }
